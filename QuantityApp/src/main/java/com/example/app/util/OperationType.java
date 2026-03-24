655c5294d4b4ffd0048e19d6ebeb4afcec1610bc
Aman Verma <122087287+avaman7860gla@users.noreply.github.com>
2026-03-24 16:06:51 +0530

    Delete QuantityApp directory

--- QuantityApp/src/main/java/com/example/app/util/OperationType.java
+++
@@ -1,7 +0,0 @@
-package com.example.app.util;
-
-public enum OperationType {
-    ADD,
-    COMPARE,
-    CONVERT
-}
--- QuantityApp/src/main/resources/db/schema.sql
+++
@@ -1,7 +0,0 @@
---Run before running main file
-CREATE DATABASE quantity_db;
-
-use quantity_db;
-
--- Run after run main file
-select * from quantity_measurement;
