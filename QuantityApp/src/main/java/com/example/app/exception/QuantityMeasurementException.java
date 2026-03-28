d8cef19468e167f15f98e5da825d9042538ec197
Aman Verma <122087287+avaman7860gla@users.noreply.github.com>
2026-03-28 15:13:25 +0530

    Delete QuantityApp directory

--- QuantityApp/src/main/java/com/example/app/exception/QuantityMeasurementException.java
+++
@@ -1,8 +0,0 @@
-package com.example.app.exception;
-
-public class QuantityMeasurementException extends RuntimeException {
-
-    public QuantityMeasurementException(String message) {
-        super(message);
-    }
-}
